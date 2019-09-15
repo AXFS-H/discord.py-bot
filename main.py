261afba7fef9615c38a811f85c661e6ecae03713
KrystianJonca <krystianjonca43@gmail.com>
2019-09-15 16:30:57 +0200

    Change code style to PEP 8

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import json
 import requests
 import random
-import datetime
+
 from config import *
 
 client = discord.Client()
@@ -12,24 +12,29 @@ prefix = '!'
 limit = 50
 search = 'ricardo_milos'
 
+
 @client.event
 async def on_ready():
-  print('Ricardo Milos is ready to dance!')
-  await client.change_presence(status=discord.Status.idle, activity=discord.Game("!ricardo"))
+    print('Ricardo Milos is ready to dance!')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("!ricardo"))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user: return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('%sricardo' % prefix):
-    req = requests.get('https://api.tenor.com/v1/search?media_filter=minimal&key=%s&q=%s&limit=%s' % (api_key, search, limit))
-    if req.status_code == 200:
-      results = json.loads(req.content)["results"]
-      choice = random.choice(results)
-      fileUrl = str(choice.get("media")[0].get("tinygif").get("url"))
-      embed = discord.Embed(title="Ricardo is dancing :man_dancing: ", color=0xe63e58).set_footer(text="Ricardo Milos", icon_url="https://i.imgur.com/VJ8oioL.jpg").set_image(url=fileUrl)
-      return await message.channel.send(embed=embed)
-    else:
-      return await message.channel.send("Sorry, something went wrong. Please try again")
+    if message.content.startswith('%sricardo' % prefix):
+        req = requests.get(
+            'https://api.tenor.com/v1/search?media_filter=minimal&key=%s&q=%s&limit=%s' % (api_key, search, limit))
+        if req.status_code == 200:
+            results = json.loads(req.content)["results"]
+            choice = random.choice(results)
+            file_url = str(choice.get("media")[0].get("tinygif").get("url"))
+            embed = discord.Embed(title="Ricardo is dancing :man_dancing: ", color=0xe63e58).set_footer(
+                text="Ricardo Milos", icon_url="https://i.imgur.com/VJ8oioL.jpg").set_image(url=file_url)
+            return await message.channel.send(embed=embed)
+        else:
+            return await message.channel.send("Sorry, something went wrong. Please try again")
 
 client.run(token)
